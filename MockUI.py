c240404d3afa0af8af92eb8de637e4030c388811
SSRDZ <sardzazas@gmail.com>
2025-02-24 10:09:59 +0700

    Set up

---
+++ MockUI.py
@@ -0,0 +1,26 @@
+from fasthtml.common import * # type: ignore
+from datetime import datetime
+from Mock_Code_for_Project import *# type: ignore
+
+app, rt = fast_app() # type: ignore
+
+@rt('/')
+def get():
+    return Titled("ทัวร์แบบจัดเอง",
+        Form(#ช่องพิมพ์ # type: ignore
+            P("วันที่เริ่มทัวร์"),
+            Label(Input(id="startDate",placeholder="วัน/เดือน/ปี")),
+            P("วันที่จบทัวร์"),
+            Label(Input(id="endDate",placeholder="วัน/เดือน/ปี")),
+            P("เลือกสถานที่ท่องเที่ยว"),
+            Label(Input(id="location",placeholder="ไปเที่ยวไหนดี...")),
+
+            Button("ตกลง",type="submit",hx_post="/CreateCustomizedTour"),
+        ))
+
+@rt('/CreateCustomizedTour')
+def post(startDate,endDate,location):
+    if(location != None and startDate != None and endDate != None):
+        website.RequestCreateTour(str(location))
+    
+serve()
---
+++ Mock_Code_for_Project.py
@@ -0,0 +1,78 @@
+#Mock Code for User's tour customizing and staff confirming
+from dataclasses import dataclass
+@dataclass
+class Website:
+    # Singleton instance
+    _instance = None
+    
+    # Class-level variables
+    pendingTour = []
+    tourManager = None
+    
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(Website, cls).__new__(cls, *args, **kwargs)
+        return cls._instance
+
+    def __init__(self):
+        self.tourManager = TourManager()
+
+    def RequestCreateTour(self,name):
+        self.pendingTour.append(self.tourManager.CreateCustomizedTour(name))
+
+    def SearchTour(self):
+        return "TourProgram Instance"
+    
+    def SearchPendingTour(self):
+        return self.pendingTour
+    
+    def ConfirmTour(self):
+        pass
+
+class TourManager:
+    tourProgram = []
+    def __init__(self):
+        self.tourProgram.append(TourProgram("Tour1"))
+        self.tourProgram.append(TourProgram("Tour2"))
+
+    def CreateCustomizedTour(self,name):
+        t = TourProgram(name)
+        print("Created",name)
+        return t
+
+class TourProgram:
+    Travelling = []
+    def __init__(self,name):
+        self.name = name
+
+class Travelling:
+    startLoacation = None
+    endLocation = None
+    def __init__(self):
+        pass
+
+class Account:
+    def __init__(self):
+        pass
+
+class User(Account):
+    def __init__(self):
+        pass
+
+    def CreateTour(self):
+        website.RequestCreatTour()
+
+    def talk(self):
+        print("Created User")
+
+class Staff(Account):
+    def __init__(self):
+        pass
+
+    def SearchPendingTour(self,tourName):
+        pass
+
+    def ConfirmTour(self,tourInstance):
+        pass
+ 
+website = Website()
