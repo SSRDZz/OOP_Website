baf94c5523db9dc4c1190bc9283982662e4ae039
SSRDZ <sardzazas@gmail.com>
2025-02-24 22:53:46 +0700

    Log in success

--- LogIn.py
+++ LogIn.py
@@ -1,29 +1,29 @@
 from fasthtml.common import *  # type: ignore
 from BackEnd import *  # type: ignore
-from CustomizeTour import * 
+from CustomizeTour import *
 app, rt = fast_app()  # type: ignore
 
 @rt('/')
-def get(): #First Time in Website
-    return Titled("Sign In",Form(
+def get():  # First time in Website
+    return Titled("Sign In", Form(
                 P("UserName"),
-                Label(Input(id="userName", type="type", placeholder="enter username")),
+                Label(Input(id="userName", type="text", placeholder="enter username")),  # type="text" for username
                 P("PassWord"),
-                Label(Input(id="password", type="type", placeholder="enter password")),
-                A("No Account?",onclick = "location.href='/SignUpPage'"),
+                Label(Input(id="password", type="password", placeholder="enter password")),  # type="password" for password
+                A("No Account?", onclick="location.href='/SignUpPage'"),
                 Div(Button("Sign In", type="button", hx_post="/SignInBackEnd")),
                 method="post",
                 onkeydown="if(event.key==='Enter'){event.preventDefault();}"
                 ))
 
-@rt('/SignInAgain') #Come back from sign In
+@rt('/SignInAgain')  # Come back from sign In
 def get():
-    return Titled("Sign In",Form(
+    return Titled("Sign In", Form(
                 P("UserName"),
-                Label(Input(id="userName", type="type", placeholder="enter username")),
+                Label(Input(id="userName", type="text", placeholder="enter username")),  # type="text" for username
                 P("PassWord"),
-                Label(Input(id="password", type="type", placeholder="enter password")),
-                A("No Account?",onclick = "location.href='/SignUpPage'"),
+                Label(Input(id="password", type="password", placeholder="enter password")),  # type="password" for password
+                A("No Account?", onclick="location.href='/SignUpPage'"),
                 Div(Button("Sign In", type="button", hx_post="/SignInBackEnd")),
                 method="post",
                 onkeydown="if(event.key==='Enter'){event.preventDefault();}"
@@ -31,26 +31,26 @@ def get():
 
 @rt('/SignUpPage')
 def get():
-    return Titled("Sign Up",Form(
+    return Titled("Sign Up", Form(
                 P("UserName"),
-                Label(Input(id="newUserName", type="type", placeholder="Enter username")),
+                Label(Input(id="newUserName", type="text", placeholder="Enter username")),  # type="text" for username
                 P("PassWord"),
-                Label(Input(id="newPassword", type="type", placeholder="Enter password")),
+                Label(Input(id="newPassword", type="password", placeholder="Enter password")),  # type="password" for password
                 Button("Sign Up", type="button", hx_post="/SignUpBackEnd",onclick="location.href='/SignInAgain'"),
                 method="post",
                 onkeydown="if(event.key==='Enter'){event.preventDefault();}"
                 ))
 
-@rt('/SignInBackEnd',methods=["POST"])
-def post(userName,password):
-    if(website.TryLogIn(userName,password) == True):
+@rt('/SignInBackEnd', methods=["POST"])
+def post(userName : str, password : str):
+    if website.TryLogIn(userName, password):
         return "Sign in success"
     else:
         return "Log In Fail"
-    
-@rt('/SignUpBackEnd',methods=["POST"])
-def post(newUserName,newPassword):
-    website.create_account(newUserName,newPassword)
+
+@rt('/SignUpBackEnd', methods=["POST"])
+def post(newUserName : str, newPassword : str):
+    website.create_account(newUserName, newPassword)
     return "Sign up success"
 
 serve()
